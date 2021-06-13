aca97e341dd746de6276310074dc7fed08dc3645
Rajkamal <rajkamal.smit@gmail.com>
2021-06-13 23:18:28 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./App.css";
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <h2>welcome</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
